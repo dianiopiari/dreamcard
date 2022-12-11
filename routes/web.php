38b8bcc1b8e79d638c457ed7dd1c6297cf510773
d opiari <dianiopiari@gmail.com>
2022-12-11 15:47:31 +0800

    init

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,17 @@
 */
 
 use Illuminate\Support\Facades\Route;
+// Route::get('/', function() {
+//     return 'Hello World';
+// });
 Route::get('/','DreamController@index');
 Route::get('/{slug}','DreamController@listPerGroup');
 Route::get('/{group_slug}/{slug}/{cat?}','DreamController@listAlbumCategori');
+// Route::get('clear/artisan', function() {
+//     Artisan::call('cache:clear');
+//     Artisan::call('route:cache');
+//     Artisan::call('config:cache');
+//     Artisan::call('view:clear');
+//     return 'Application cache, route, config & view has been cleared';
+// });
+
