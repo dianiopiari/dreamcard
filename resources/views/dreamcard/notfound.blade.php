203d91db2bfb3a7f031283452fd3c0122b14fac6
d opiari <dianiopiari@gmail.com>
2022-12-11 16:58:09 +0800

    update google analytic

--- resources/views/dreamcard/notfound.blade.php
+++ resources/views/dreamcard/notfound.blade.php
@@ -21,7 +21,15 @@
 	<!-- vendor css -->
 	<link rel="stylesheet" href="{{asset('/theme/ablepro/assets/css/style.css')}}">
 
+        <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-1VQDNKKF6L"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
+        gtag('config', 'G-1VQDNKKF6L');
+    </script>
 
 </head>
 <body class="">
