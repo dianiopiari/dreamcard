b98cee877c31d347ab23d4e312cc7ab55c9d18ba
d opiari <dianiopiari@gmail.com>
2022-12-30 16:02:00 +0800

    update name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,7 +59,7 @@
 					    <a href="{{config('app.url')}}/privacy-policy" class="nav-link "><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext">privacy Policy</span></a>
 					</li>
                     <li class="nav-item">
-					    <a href="{{config('app.url')}}/terms-condition" class="nav-link "><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext">privacy Policy</span></a>
+					    <a href="{{config('app.url')}}/terms-condition" class="nav-link "><span class="pcoded-micon"><i class="feather icon-home"></i></span><span class="pcoded-mtext">Terms & Conditions </span></a>
 					</li>
                 </ul>
 			</div>
