7787ad6af8a45937450f3aa0715c042badfe86f7
d opiari <dianiopiari@gmail.com>
2023-01-02 21:34:34 +0800

    update

--- resources/views/dreamcard/album.blade.php
+++ resources/views/dreamcard/album.blade.php
@@ -248,12 +248,6 @@
 		<!-- [ Main Content ] end -->
 	</div>
 </div>
-<div class="alert alert-warning alert-dismissible fade show" role="alert">
-    <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-      <span aria-hidden="true">&times;</span>
-    </button>
-</div>
 <!-- [ Main Content ] end -->
     <!-- Warning Section Ends -->
 
